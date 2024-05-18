57a8f0ade5d1187d44a423e034e9e3b9ef98e8c2
dennis githinji <wakahiad@gmail.com>
2024-05-18 13:22:45 +0300

    project init and redis config bean

---
+++ src/main/java/org/densoft/rediscrud/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package org.densoft.rediscrud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6380);
+        return new JedisConnectionFactory(configuration);
+    }
+}
